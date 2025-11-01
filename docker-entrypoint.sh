25e8ec6f5764f0fb0afd58b5c650793bddc8a0b5
AstouMba <assalih1303@gmail.com>
2025-11-01 19:21:02 +0000

    correction

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,14 +10,14 @@ echo "Database is up - executing migrations"
 php artisan migrate --force
 
 # Vérifier si un client password existe
-if [ -z "$(php artisan tinker --execute='echo \App\Models\OauthClient::where("password_client", true)->first()->id ?? ""')" ]; then
+if [ -z "$(php artisan tinker --execute='echo \Laravel\Passport\Client::where("password_client", true)->first()->id ?? ""')" ]; then
   echo "Installing Passport clients..."
   php artisan passport:install --force
 fi
 
 # Récupérer le client password
-PASSPORT_PASSWORD_CLIENT_ID=$(php artisan tinker --execute='echo \App\Models\OauthClient::where("password_client", true)->first()->id;')
-PASSPORT_PASSWORD_CLIENT_SECRET=$(php artisan tinker --execute='echo \App\Models\OauthClient::where("password_client", true)->first()->secret;')
+PASSPORT_PASSWORD_CLIENT_ID=$(php artisan tinker --execute='echo \Laravel\Passport\Client::where("password_client", true)->first()->id;')
+PASSPORT_PASSWORD_CLIENT_SECRET=$(php artisan tinker --execute='echo \Laravel\Passport\Client::where("password_client", true)->first()->secret;')
 
 export PASSPORT_PASSWORD_CLIENT_ID
 export PASSPORT_PASSWORD_CLIENT_SECRET
